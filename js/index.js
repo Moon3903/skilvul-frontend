0029b70af04726d5196728bce6beea651f5622c4
unknown <zulfikar.fauzul.akbar@gmail.com>
2021-10-27 13:43:34 +0700

    add swal for feedback and newsletter

--- js/index.js
+++ js/index.js
@@ -1,22 +1,69 @@
 import * as helpers from "./helper.js";
 
 const list = document.querySelector("#popular").querySelectorAll(".item");
-console.log(list[0]);
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const emailNews = document.querySelector("#emailNewsletter");
+const feedback = document.querySelector("#feedback");
 
+document
+  .getElementById("submitFeedback")
+  .addEventListener("click", sendFeedback);
+document
+  .getElementById("submitNewsletter")
+  .addEventListener("click", addNewsletter);
 const getPosts = await helpers.getPosts();
 
-function popularList(){
-    for(let i=0;i<list.length;i++){
-        const thumb = list[i].querySelector("img");
-        const title = list[i].querySelector("h4");
-        const route = list[i].querySelector("a");
-        const date = list[i].querySelector("span")
+function popularList() {
+  for (let i = 0; i < list.length; i++) {
+    const thumb = list[i].querySelector("img");
+    const title = list[i].querySelector("h4");
+    const route = list[i].querySelector("a");
+    const date = list[i].querySelector("span");
 
-        date.innerText = getPosts[i%6].time;
-        route.href = "detail_artikel.html?key="+getPosts[i%6].key;
-        title.innerText = getPosts[i%6].title;
-        thumb.src = getPosts[i%6].thumb;
-    }
+    date.innerText = getPosts[i % 6].time;
+    route.href = "detail_artikel.html?key=" + getPosts[i % 6].key;
+    title.innerText = getPosts[i % 6].title;
+    thumb.src = getPosts[i % 6].thumb;
+  }
+}
+
+function sendFeedback(e) {
+  e.preventDefault();
+  if (name.value == "" || email.value == "" || feedback.value == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please fill the feedback form first",
+    });
+    return;
+  }
+  Swal.fire({
+    icon: "success",
+    title: "Feedback send",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+}
+
+function addNewsletter(e) {
+  e.preventDefault();
+  const regexEmail = /^[A-Za-z.]+[\w\d]+@\w+\.\w+/;
+  console.log(emailNews.value);
+  if (!emailNews.value.match(regexEmail)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please input valid email",
+    });
+    return;
+  }
+  Swal.fire({
+    icon: "success",
+    title: "Newsletter added",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 }
 
 popularList();
